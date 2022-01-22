647a6fcba365e2db919994ca39f9796a476b154a
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-22 11:37:38 -0300

    Desenvolvimento

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import Container from "./styles"
+
+function Header () {
+    return(
+        <Container>
+            <Content>
+                <LogoContainer>
+                    <LogoIcon src="/images/asasasdadas">
+
+                    </LogoIcon>
+                    <Logo src="/images/asfasdasdasd">
+
+                    </Logo>
+                </LogoContainer>
+                <OptionsContainer>
+                    <Icon></Icon>
+                    <Avatar>
+                        <img src="/images/asasasasass" />
+                    </Avatar>
+                </OptionsContainer>
+            </Content>
+        </Container>
+
+    )
+}
+
+
+export default Header
